990357aa71514652ccb4255c4866b0a3abab0b84
locemar <marcelon27@gmail.com>
2024-05-07 22:07:43 -0300

    Feat: UserEntity class - unit tests

---
+++ src/domain/Entities/tests/test_user_entity.py
@@ -0,0 +1,64 @@
+import pytest
+
+from src.domain.Entities.user_entity import UserEntity
+
+
+@pytest.fixture()
+def user_entity() -> UserEntity:
+    return UserEntity(name='test', email='test@email.com', password='secret')
+
+
+def test_user_entity_exception_name(user_entity: UserEntity):
+    with pytest.raises(TypeError) as e:
+        user_entity.name = 2
+    assert str(e.value) == 'Name must be a string'
+
+
+def test_user_entity_exception_name_cannot__str_empty(user_entity: UserEntity):
+    with pytest.raises(ValueError) as e:
+        user_entity.name = ''
+    assert str(e.value) == 'Name cannot be empty'
+
+
+def test_user_entity_exception_name_none(user_entity: UserEntity):
+    with pytest.raises(TypeError) as e:
+        user_entity.name = {}
+    assert str(e.value) == 'Name must be a string'
+
+
+def test_user_entity_exception_email_not_str(user_entity: UserEntity):
+    with pytest.raises(TypeError) as e:
+        user_entity.email = 1
+    assert str(e.value) == 'Email must be a string'
+
+
+def test_user_entity_exception_email_cannot_str_empty(user_entity: UserEntity):
+    with pytest.raises(ValueError) as e:
+        user_entity.email = ''
+    assert str(e.value) == 'Email cannot be empty'
+
+
+def test_user_entity_exception_email():
+    with pytest.raises(ValueError, match='Invalid email address') as e:
+        UserEntity(name='test', email='test@email', password='secret')
+    assert str(e.value) == 'Invalid email address'
+
+
+def test_user_entity_exception_password_cannot_empty_str(
+    user_entity: UserEntity,
+):
+    with pytest.raises(ValueError, match='Password cannot be empty') as e:
+        user_entity.password = ''
+    assert str(e.value) == 'Password cannot be empty'
+
+
+def test_user_entity_exception_password_not_str(user_entity: UserEntity):
+    with pytest.raises(TypeError) as e:
+        user_entity.password = 1
+    assert str(e.value) == 'Password must be a string'
+
+
+def test_user_entity(user_entity: UserEntity):
+    assert user_entity.email == 'test@email.com'
+    assert user_entity.password == 'secret'
+    assert user_entity.name == 'test'
---
+++ src/domain/Entities/user_entity.py
@@ -0,0 +1,61 @@
+import re
+
+
+class UserEntity:
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = password
+
+    def __is_valid_email(self, value) -> str | None:
+        regex = re.compile(
+            r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+'
+        )
+        r = re.fullmatch(regex, value)
+        print('r', r)
+        return r
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def email(self):
+        return self._email
+
+    @property
+    def password(self):
+        return self._password
+
+    @name.setter
+    def name(self, value):
+        if type(value) is not str:
+            raise TypeError('Name must be a string')
+
+        if str(value) == '':
+            raise ValueError('Name cannot be empty')
+
+        self._name = value
+
+    @email.setter
+    def email(self, value):
+        if type(value) is not str:
+            raise TypeError('Email must be a string')
+
+        if str(value) == '':
+            raise ValueError('Email cannot be empty')
+
+        if not self.__is_valid_email(value):
+            raise ValueError('Invalid email address')
+
+        self._email = value
+
+    @password.setter
+    def password(self, value):
+        if type(value) is not str:
+            raise TypeError('Password must be a string')
+
+        if str(value) == '':
+            raise ValueError('Password cannot be empty')
+
+        self._password = value
